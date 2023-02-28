7d5b5038c992f10db597e1c3250ac49536540add
OM3333 <oskar.maksim@skni.umcs.pl>
2023-02-28 18:42:44 +0100

    Init commitafqfeqgeq

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Segment s = new Segment(new Point(100,100),new Point(200,200));
         System.out.println(s.toSvg()+"\n");
-        System.out.println(Segment.perpendicular(s,new Point(100,200)).get(0).toSvg());
-        System.out.println(Segment.perpendicular(s,new Point(100,200)).get(1).toSvg());
+        System.out.println(Segment.perpendicular(s,new Point(100,200))[0].toSvg());
+        System.out.println(Segment.perpendicular(s,new Point(100,200))[1].toSvg());
     }
 }
--- src/Segment.java
+++ src/Segment.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.Locale;
 
 public class Segment {
     Point start;
@@ -17,21 +18,20 @@ public class Segment {
         return end;
     }
 
-    public static List<Segment> perpendicular(Segment s, Point p){
+    public static Segment[] perpendicular(Segment s, Point p){
         int xP = s.end.x-s.start.x;
         int yP = s.end.y-s.start.y;
         Point startP = new Point(-yP+p.x,xP+p.y);
         Point startP2 = new Point(yP+p.x,-xP+p.y);
         Segment result1 = new Segment(startP,p);
         Segment result2 = new Segment(startP2,p);
-        return List.of(
-                result1,
-                result2
-        );
+        Segment results[] = {result1,result2};
+        return results;
+
     }
 
     public String toSvg(){
-        return String.format("<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" style=\"stroke:rgb(%d,%d,%d);stroke-width:2\"/>",start.x, start.y, end.x, end.y,start.x*10%255,end.y%255,(start.x+end.x)%255);
+        return String.format(Locale.ENGLISH, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" style=\"stroke:rgb(%d,%d,%d);stroke-width:2\"/>",start.x, start.y, end.x, end.y,start.x*10%255,end.y%255,(start.x+end.x)%255);
     }
 
     public double length()
